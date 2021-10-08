2855efd601d02459a26d001c035ab7cdbbbda947
Qiping Pan <60682078+ppppqp@users.noreply.github.com>
2021-10-08 18:53:38 -0400

    sequence align

---
+++ sequence_align/aligner.cpp
@@ -0,0 +1,170 @@
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+const double inf = numeric_limits<double>::infinity();
+class Cell {
+ public:
+  double value;
+  char trace;
+};
+class Aligner {
+ private:
+  string filename1, filename2;
+  string seqX, seqY;
+  string seqXAligned, seqYAligned;
+  vector<vector<Cell> > M, X, Y;
+  double alignScore;
+  // M: match, X: insert @ seq1, Y: insert @ seq2
+  double gapOpen, gapExt;
+
+ public:
+  Aligner(){};
+  Aligner(string _filename1, string _filename2, double _gapOpen, double _gapExt)
+      : filename1(_filename1),
+        filename2(_filename2),
+        gapOpen(_gapOpen),
+        gapExt(_gapExt) {
+    ifstream file1(_filename1), file2(_filename2);
+    file1 >> seqX;
+    file2 >> seqY;
+  };
+  void align();
+  void print();
+  void printMatrix(vector<vector<Cell> >&m);
+};
+void resizeMatrix(vector<vector<Cell> >& m, int height, int width) {
+  m.resize(height);
+  for (int i = 0; i < height; i++) {
+    m[i].resize(width);
+    for (int j = 0; j < width; j++) {
+      m[i][j].value = 0;
+      m[i][j].trace = 'N';
+    }
+  }
+}
+void calCell(Cell& c, double m, double x, double y) {
+  c.value = m;
+  c.trace = 'M';
+  if (x > c.value) {
+    c.value = x;
+    c.trace = 'X';
+  }
+  if (y > c.value) {
+    c.value = y;
+    c.trace = 'Y';
+  }
+}
+double score(char a, char b) { return (a == b) ? 1 : -1; }
+void Aligner::align() {
+  int xLength = seqX.length();
+  int yLength = seqY.length();
+  resizeMatrix(M, yLength + 1, xLength + 1);  // shape: yLength, xLength
+  resizeMatrix(X, yLength + 1, xLength + 1);
+  resizeMatrix(Y, yLength + 1, xLength + 1);
+  // initialize m matrix
+  M[0][0].value = 0;
+  for (int i = 1; i <= yLength; i++) M[i][0].value = -inf;
+  for (int j = 1; j <= xLength; j++) M[0][j].value = -inf;
+  // initialize x matrix
+  X[0][0].value = -gapOpen;
+  for (int i = 1; i <= yLength; i++) X[i][0].value = -inf;
+  for (int j = 1; j <= xLength; j++) X[0][j].value = X[0][j-1].value - gapExt;
+  // initialize y matrix
+  Y[0][0].value = -gapOpen;
+  for (int i = 1; i <= yLength; i++) Y[i][0].value = Y[i][0].value - gapExt;
+  for (int j = 1; j <= xLength; j++) Y[0][j].value =  -inf;
+  // propogate weight
+  for (int i = 1; i <= yLength; i++) {
+    for (int j = 1; j <= xLength; j++) {
+      // calculate M[i][j]
+      calCell(M[i][j], M[i - 1][j - 1].value + score(seqX[j - 1], seqY[i - 1]),
+              X[i - 1][j - 1].value + score(seqX[j - 1], seqY[i - 1]),
+              Y[i - 1][j - 1].value + score(seqX[j - 1], seqY[i - 1]));
+      // calculate X[i][j]
+      calCell(X[i][j], M[i][j-1].value - gapOpen, X[i][j-1].value - gapExt,
+              -inf);
+      // calculate Y[i][j];
+      calCell(Y[i][j], M[i-1][j].value - gapOpen, -inf,
+              Y[i-1][j].value - gapExt);
+    }
+  }
+  int i = yLength, j = xLength;
+  Cell curCell = M[i][j];
+  char cellType = 'M';
+  alignScore = M[i][j].value;
+  if (X[i][j].value > curCell.value) {
+    curCell = X[i][j];
+    cellType = 'X';
+    alignScore = X[i][j].value;
+  }
+  if (Y[i][j].value > curCell.value) {
+    curCell = Y[i][j];
+    cellType = 'Y';
+    alignScore = Y[i][j].value;
+  }
+
+  while (i > 0 || j > 0) {
+    if(cellType == 'X') curCell = X[i][j];
+    if(cellType == 'Y') curCell = Y[i][j];
+    if(cellType == 'M') curCell = M[i][j];
+    if (cellType == 'M') {
+      // match
+      seqXAligned += seqX[j - 1];
+      seqYAligned += seqY[i - 1];
+      cellType = curCell.trace;
+      i--;
+      j--;
+      continue;
+    }
+    if (cellType == 'X') {
+      // X aligned to a gap
+      seqXAligned += seqX[j - 1];
+      seqYAligned += "-";
+      cellType = curCell.trace;
+      j--;
+      continue;
+    }
+    if (cellType == 'Y') {
+      // Y aligned to a gap
+      seqXAligned += "-";
+      seqYAligned += seqY[i - 1];
+      cellType = curCell.trace;
+      i--;
+      continue;
+    }
+  }
+  reverse(seqXAligned.begin(), seqXAligned.end());
+  reverse(seqYAligned.begin(), seqYAligned.end());
+}
+void Aligner::print() {
+  cout << "Aligned Sequence Score:" << alignScore << endl;
+  cout << "Aligned Sequence Result:" << endl;
+  int resPerLine = 50;
+  assert(seqXAligned.length() == seqYAligned.length());
+  int start = 0;
+  while (start + resPerLine < seqXAligned.length()) {
+    cout << "> X: " << seqXAligned.substr(start, start + resPerLine) << endl;
+    cout << "> Y: " << seqYAligned.substr(start, start + resPerLine) << endl;
+    cout << endl;
+    start += resPerLine;
+  }
+  cout << "> X: " << seqXAligned.substr(start, seqXAligned.length() - start)
+       << endl;
+  cout << "> Y: " << seqYAligned.substr(start, seqYAligned.length() - start)
+       << endl;
+}
+void Aligner::printMatrix(vector<vector<Cell> >&m){
+    cout << "Start printMatrix: \n";
+    for(int i = 0; i < m.size(); i++){
+        for(int j = 0; j < m[0].size(); j++){
+            cout << setw(5) << m[i][j].trace << ' ';
+        }
+        cout << endl;
+    }
+    cout << "End printMatrix: \n";
+
+}
